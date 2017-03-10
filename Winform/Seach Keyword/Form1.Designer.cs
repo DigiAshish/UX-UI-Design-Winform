6f74f38815227e8f74ff3799b5ee6f0b6829a8c4
“Ashish <aashish.mohapatra@gmail.com>
2017-03-10 16:58:48 -0600

    Search Keyword in file

---
+++ Winform/Seach Keyword/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Asg4_axm160031
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFilename = new System.Windows.Forms.TextBox();
+            this.tbKeyword = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbDisplay = new System.Windows.Forms.ListBox();
+            this.Filename = new System.Windows.Forms.Label();
+            this.keyword = new System.Windows.Forms.Label();
+            this.lblNotification = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbFilename
+            // 
+            this.tbFilename.Location = new System.Drawing.Point(83, 35);
+            this.tbFilename.Name = "tbFilename";
+            this.tbFilename.Size = new System.Drawing.Size(445, 20);
+            this.tbFilename.TabIndex = 0;
+            // 
+            // tbKeyword
+            // 
+            this.tbKeyword.Location = new System.Drawing.Point(83, 72);
+            this.tbKeyword.Name = "tbKeyword";
+            this.tbKeyword.Size = new System.Drawing.Size(444, 20);
+            this.tbKeyword.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(563, 30);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(118, 24);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(563, 66);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(118, 26);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.FormattingEnabled = true;
+            this.lbDisplay.Location = new System.Drawing.Point(23, 129);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(681, 329);
+            this.lbDisplay.TabIndex = 4;
+            // 
+            // Filename
+            // 
+            this.Filename.AutoSize = true;
+            this.Filename.Location = new System.Drawing.Point(18, 38);
+            this.Filename.Name = "Filename";
+            this.Filename.Size = new System.Drawing.Size(49, 13);
+            this.Filename.TabIndex = 5;
+            this.Filename.Text = "Filename";
+            // 
+            // keyword
+            // 
+            this.keyword.AutoSize = true;
+            this.keyword.Location = new System.Drawing.Point(19, 79);
+            this.keyword.Name = "keyword";
+            this.keyword.Size = new System.Drawing.Size(48, 13);
+            this.keyword.TabIndex = 6;
+            this.keyword.Text = "Keyword";
+            // 
+            // lblNotification
+            // 
+            this.lblNotification.AutoSize = true;
+            this.lblNotification.Location = new System.Drawing.Point(89, 103);
+            this.lblNotification.Name = "lblNotification";
+            this.lblNotification.Size = new System.Drawing.Size(0, 13);
+            this.lblNotification.TabIndex = 7;
+            this.lblNotification.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 485);
+            this.Controls.Add(this.lblNotification);
+            this.Controls.Add(this.keyword);
+            this.Controls.Add(this.Filename);
+            this.Controls.Add(this.lbDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbKeyword);
+            this.Controls.Add(this.tbFilename);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFilename;
+        private System.Windows.Forms.TextBox tbKeyword;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lbDisplay;
+        private System.Windows.Forms.Label Filename;
+        private System.Windows.Forms.Label keyword;
+        private System.Windows.Forms.Label lblNotification;
+    }
+}
+
---
+++ Winform/Seach Keyword/Form1.cs
@@ -0,0 +1,97 @@
+﻿/*********************************************************************************************
+ Author - Ashish Mohapatra
+ This program Browse a file from the Local and accepts a keyword to be search in the file. Whenever there is a hit of the keyword
+ with the line in the file,This program shows the line number and the corresponding line.
+**********************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asg4_axm160031
+{
+    public partial class Form1 : Form
+    {
+        private bool stopIt = false;//To check whether "Cancel" instruction has been provided
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog(); //On click of Button, Open the file explorer
+            if (ofg.ShowDialog() == DialogResult.OK) // Show the file explorer dialog box.
+            {
+                string file = ofg.FileName; //Store the path of the file name selected to a string variable
+                tbFilename.Text = file;      // Show the file name in the text box for reference          
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread t = new System.Threading.Thread(DisplayResult);//Initiate Threading
+            if (btnSearch.Text == "Search") //If the button is "Search" enabled
+            {
+                stopIt = false; //Continue showing the result
+                t.IsBackground = true;
+                t.Start();// Start Threading
+                lbDisplay.HorizontalScrollbar = true;// Enable horizontal Scrolling bar
+                lblNotification.Visible = false; //Disable the notification TextBox
+            }
+            else
+            {
+                stopIt = true; //If the button is "Cancel" Enabled then enabled the variable to stop the search
+            }
+        }
+        void DisplayResult()
+        {
+            string file = tbFilename.Text; //Get the file name
+            string keyword = tbKeyword.Text; //Get the keyword to be searched
+            Boolean NotFound = false; //Initialize NotFound string to be false
+            int i = 1;
+            if (lbDisplay.Items != null && lbDisplay.Items.Count > 0)
+            {
+                this.Invoke((MethodInvoker)(() => lbDisplay.Items.Clear())); //Clear the list box for multiple search result
+            }
+            string[] curItem = System.IO.File.ReadAllLines(file);//Read all lines from the Text file
+            this.Invoke((MethodInvoker)(() => lbDisplay.Items.Add("Line Number:" + "\t" + "\t" + "Line")));//Show the header
+            try
+            {
+                foreach (var item in curItem)
+                {
+                    this.Invoke((MethodInvoker)(() => btnSearch.Text = "Cancel"));//Change the button from "Search" to "Cancel"
+                    if (string.IsNullOrWhiteSpace(item)) //if any line contains blank line then ignore it.
+                    {
+                        continue;
+                    }
+                    if (!stopIt) //If not instructed to stop the search do teh following
+                    {
+                        if (item.ToLower().Contains(keyword.ToLower()))//if the line contains the keyword
+                        {
+                            System.Threading.Thread.Sleep(100);//Delay the search by 100ms
+                            this.Invoke((MethodInvoker)(() => lbDisplay.Items.Add(i + "\t" + "\t" + item))); //Display the line number and the whole line
+                            this.Invoke((MethodInvoker)(() => NotFound = true)); //Change the notFound variable to be true
+                        }
+                    }
+                    i++;
+                }
+                if(NotFound==false)//if the notFound variable is still false that means there is no search result
+                {
+                    this.Invoke((MethodInvoker)(() => lblNotification.Visible = true)); //Enable the notification text box
+                    this.Invoke((MethodInvoker)(() => lblNotification.Text = "No Results were found")); //Show the notification that No result found
+                }
+            }catch (System.IO.IOException e){
+            }
+            finally
+            {
+                this.Invoke((MethodInvoker)(() => btnSearch.Text = "Search"));//Finally change the button back to "Search"
+            }
+        }
+    }
+}
